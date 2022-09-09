0254f4645594f6df89aba581fdb6c0cc25e05f2d
Guilherme Aureliano <49173189+GuilhermeAureliano@users.noreply.github.com>
2022-09-08 21:43:27 -0300

    Projeto criado

---
+++ backend/src/test/java/com/devsuperior/dsmeta/DsmetaApplicationTest.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DsmetaApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
